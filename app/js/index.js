4001186a2c07745357cc61ff2021c0aad3596d70
eduardodsn <carlosedsb0110@gmail.com>
2021-01-05 12:45:44 -0300

    Cards dinamicos criados

--- app/js/index.js
+++ app/js/index.js
@@ -9,23 +9,30 @@ search_button.addEventListener('click', getMlData);
 
 
 function getMlData() {
-    console.log('hello boya')
-    let product_name = 'acer';//product_input_text.value;
+    container.innerHTML = "";
+    let product_name = "teclado mecânico" //product_input_text.value;
+
     osmosis
     .get(`https://lista.mercadolivre.com.br/${product_name}`)
     .find('div.ui-search-result__wrapper')
     .set({
-        title: 'h2.ui-search-item__title.ui-search-item__group__element',
-        img: 'img.ui-search-result-image__element@src'
+        img: 'img.ui-search-result-image__element@src',
+        title: 'h2.ui-search-item__title',
+        priceFraction: 'span.price-tag-fraction',
+        priceCents: 'span.price-tag-cents'
     })
     .data(data => {
         console.log(data);
     });
 }
 
-// function appendNewCard(data) {
-//     const div = document.createElement('div')
-//     div.classList = 'card'
-//     div.innerHTML = data
-//     container.appendChild(div)
-// }
+function appendNewCard(data) {
+    const div = document.createElement('div')
+    div.classList = 'card'
+    div.innerHTML = `
+                        <img src="${data.img}" alt="Imagem do produto">
+                        <p>${data.title}</p>
+                        <p>R$${data.priceFraction},${data.priceCents}</p>
+                    `
+    container.appendChild(div)
+}
